cb9acd1c08e1815023b7398dc46f91275e12ebb5
McGregor Joyner <113465276+mcgsjoyner@users.noreply.github.com>
2024-07-26 18:21:07 -0600

    disable rather than hide sliders (#28)

--- src/components/WindDirPicker.vue
+++ src/components/WindDirPicker.vue
@@ -1,15 +1,26 @@
 <script setup lang="ts">
-    import {ref, watch} from "vue"
+    import {ref, watch, reactive} from "vue"
     var sliderText = ref("");
     var sliderValue = ref(360);
 
+    const props = defineProps({
+      disabled: Boolean,
+    });
+
     function get_slider_text(val: number) {
-        return 'Wind Dir: ' + (val.toString().padStart(3, '0') + '\u00B0');
+        var text = 'Wind Dir: '
+        var formattedDirection = val.toString().padStart(3, '0') + '\u00B0';
+        formattedDirection = props.disabled ? "VARIABLE" : formattedDirection;
+        return `Wind Dir: ${formattedDirection}`;
     };
     watch(sliderValue, async (newVal, oldVal) => {
         sliderText.value = get_slider_text(newVal);
+        styleObject.background = props.disabled ? "#cccccc" : "rgba(0.0, 0.0, 255.0)";
     })
-    sliderText.value = get_slider_text(sliderValue.value);
+    watch(() => props.disabled, async (newVal, oldVal) => {
+        styleObject.background = newVal ? "#cccccc" : "rgba(0.0, 0.0, 255.0)";
+        sliderText.value = get_slider_text(sliderValue.value);
+    })
 
     const emit = defineEmits<{
         (e: 'emitWindDir', sliderValue: number): void
@@ -17,6 +28,11 @@
     const onChange = () => {
         emit('emitWindDir', sliderValue.value);
     }
+    const styleObject = reactive({
+        background: "#cccccc",
+        accentColor: "#cccccc",
+    })
+    sliderText.value = get_slider_text(sliderValue.value);
     onChange();
 </script>
 
@@ -28,6 +44,8 @@
             v-model.number="sliderValue"
             @change="onChange"
             class="custom-slider custom-slider-wind-dir"
+            :style="styleObject"
+            :disabled="disabled"
             min=10
             max=360
             step=10
@@ -35,10 +53,3 @@
         <span id="windDirPickerSpan" v-text="sliderText"></span>
     </label>
 </template>
-
-<style>
-    .custom-slider-wind-dir {
-      accent-color: rgba(0.0, 0.0, 255.0);
-      background: rgba(0.0, 0.0, 255.0);
-    }
-</style>
