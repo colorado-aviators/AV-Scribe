94b1b26d851d1dfc153f9e43d25eea5f75b632c7
McGregor Joyner <113465276+mcgsjoyner@users.noreply.github.com>
2024-07-31 16:00:46 -0600

    exclude null values from transcript (#52)

--- src/components/Transcript.vue
+++ src/components/Transcript.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
     import {ref, watch} from "vue"
+    import TranscriptDownloader from "./TranscriptDownloader.vue"
 
+    const atisText = ref("");
+    const disablingCoverages = ["SKC", "NCD", "CLR", "VV"];
     const props = defineProps({
         airport: {type: String, required: true},
-        information: {type: String, required: true},
+        information: {type: String, required: false},
         time:  {type: String, required: true},
         windCondition: {type: String, required: true},
         windVel: {type: Number, required: true},
@@ -17,52 +20,51 @@
         altimeter:  {type: Number, required: true},
         densityAltitude:  {type: Number, required: true},
     });
-    const disablingCoverages = ["SKC", "NCD", "CLR", "VV"];
 
-    const ADDRESS = "https://mcgsjoyner.github.io/AVWX-Scribe/";
-    var atisText = ref("");
-    watch(() =>
-    [
-        props.visibility,
-        props.cloudCoverage,
-        props.ceiling,
-        props.airport,
-        props.information,
-        props.time,
-        props.altimeter,
-        props.windVel,
-        props.windCondition,
-        props.windGust,
-        props.windDir,
-        props.temperature,
-        props.dewpoint,
-    ] as const,
-    ([
-        visibilityVal,
-        cloudCoverageVal,
-        ceilingVal,
-        airportVal,
-        informationVal,
-        timeVal,
-        altimeterVal,
-        windVelVal,
-        windConditionVal,
-        windGustVal,
-        windDirVal,
-        temperatureVal,
-        dewpointVal,
-    ]) =>
-    {
+    function get_airport_chunk() {
+        return props.airport;
+    }
+
+    function get_information_chunk() {
+        return props.information ? `INFO ${props.information}` : null;
+    }
+
+    function get_time_chunk() {
+        return props.time.replace(/\s/g, '') + 'Z';
+    }
+
+    function get_wind_chunk() {
         var windChunk = '00000';
-        if (windVelVal > 0) {
-            console.log(windConditionVal)
-            windChunk = windConditionVal == "Variable" ? 'VRB' : windDirVal.toString().padStart(3, '0');
-            windChunk += windVelVal.toFixed(0).padStart(2, '0');
-            if (windGustVal != 0) {
-                windChunk += `G${windGustVal.toFixed(0)}`;
+        if (props.windVel > 0) {
+            let windDirString = props.windDir.toString().padStart(3, '0');
+            let windVelString = props.windVel.toFixed(0).padStart(2, '0');
+            let windGustString = "G" + props.windGust.toFixed(0);
+            let windVariable = props.windCondition == "Variable";
+
+            windChunk = windVariable ? 'VRB' : windDirString;
+            windChunk += windVelString;
+            if (props.windGust != 0) {
+                windChunk += windGustString;
             }
         }
         windChunk += 'KT';
+        return windChunk;
+    }
+
+    function get_visibility_chunk() {
+        return props.visibility.toFixed(0) + 'SM';
+    }
+
+    function get_cloud_chunk() {
+        let noClouds = disablingCoverages.includes(props.cloudCoverage);
+        var cloudChunk = props.cloudCoverage;
+        if (!noClouds) {
+            cloudChunk += Math.round(props.ceiling / 100).toFixed(0).padStart(3, '0');
+        }
+        return cloudChunk;
+    }
+
+    function get_temperatures_chunk() {
         function format_temp(val: number) {
             const negative = val.toFixed(0).startsWith('-');
             var formatted = val.toFixed(0).replace('-','').padStart(2, '0');
@@ -71,153 +73,68 @@
             }
             return formatted;
         }
+        return [format_temp(props.temperature), format_temp(props.dewpoint)].join("/");
+    }
 
-        var cloudChunk = cloudCoverageVal;
-        if (!disablingCoverages.includes(cloudCoverageVal)) {
-            cloudChunk += Math.round(ceilingVal / 100).toFixed(0).padStart(3, '0');
-        }
+    function get_altimeter_chunk() {
+        return 'A' + props.altimeter.toFixed(2).replace('.', '');
+    }
 
-        const chunks = [
-            airportVal,
-            `INFO ${informationVal}`,
-            timeVal.replace(/\s/g, '') + 'Z',
-            windChunk,
-            (visibilityVal).toFixed(0) + 'SM',
-            cloudChunk,
-            `${format_temp(temperatureVal)}/${format_temp(dewpointVal)}`,
-            'A' + (altimeterVal).toFixed(2).replace('.', ''),
+    function update_atis_text() {
+        let chunks = [
+            get_airport_chunk(),
+            get_information_chunk(),
+            get_time_chunk(),
+            get_wind_chunk(),
+            get_visibility_chunk(),
+            get_cloud_chunk(),
+            get_temperatures_chunk(),
+            get_altimeter_chunk(),
         ];
-        atisText.value = chunks.join(' ');
-    });
-
-    function get_airport_text() {
-        return `Airport: ${props.airport}`;
+        let isNotNull = (value: any) => value != null;
+        atisText.value = chunks.filter(isNotNull).join(' ');
     }
 
-    function get_information_text() {
-        return `Information: ${props.information}`
-    }
-
-    function get_time_text() {
-        return `Time: ${props.time} Z`;
-    }
-
-    function get_wind_text() {
-        var windText = 'Wind: ';
-        if (props.windVel == 0) {
-            windText += 'Calm';
-            return windText;
-        }
-        if (props.windCondition) {
-            windText += 'VRB';
-        }
-        else {
-            windText += props.windDir.toString().padStart(3, '0') + '&deg;'
-        }
-        let val = props.windVel.toFixed(0);
-        windText += ` @ ${val} KT`;
-        if (props.windGust > 0) {
-            val = props.windGust.toFixed(0);
-            windText += ` G ${val} KT`;
-        }
-        return windText;
-    }
-
-    function get_visibility_text() {
-        let val = props.visibility.toFixed(0);
-        return `Visibility: ${val} SM`;
-    }
-
-    function get_cloud_text() {
-        let cloudChunk = props.cloudCoverage;
-        if (!disablingCoverages.includes(props.cloudCoverage)) {
-            let val = Math.round(props.ceiling);
-            cloudChunk += `@ ${val} ft AGL`;
-        }
-        return `Cloud Coverage: ${cloudChunk}`;
-    }
-
-    function get_temperature_text() {
-        let val = props.temperature.toFixed(0);
-        return `Temperature: ${val}&deg;C`;
-    }
-
-    function get_dewpoint_text() {
-        let val = props.dewpoint.toFixed(0);
-        return `Dew Point: ${val}&deg;C`;
-    }
-
-    function get_spread_text() {
-        let spread = props.temperature - props.dewpoint;
-        let val = spread.toFixed(0);
-        return `Spread: ${val}&deg;C`
-    }
-
-    function get_altimeter_text() {
-        let val = props.altimeter.toFixed(2);
-        return `Altimeter Setting: ${val} inHg`;
-    }
-
-    function get_density_altitude_text() {
-        let val = props.densityAltitude.toFixed(0);
-        return `Density Altitude (est.): ${val} ft`;
-    }
-
-    function download_transcript() {
-        // Create element with <a> tag
-        const link = document.createElement("a");
-
-        // Add file name
-        var tags = [
+    watch(
+        () =>
+        [
+            props.visibility,
+            props.cloudCoverage,
+            props.ceiling,
             props.airport,
             props.information,
-            props.time.replace(/\s/g, "") + 'Z',
-        ]
-        const filename = `information-${tags.join('-')}.html`.toLowerCase()
-        link.download = filename;
-
-        // Create a blob object with the file content which you want to add to the file
-        var content = [
-            atisText.value,
-            '',
-            get_airport_text(),
-            get_information_text(),
-            get_time_text(),
-            get_wind_text(),
-            get_visibility_text(),
-            get_cloud_text(),
-            get_temperature_text(),
-            get_dewpoint_text(),
-            get_spread_text(),
-            get_altimeter_text(),
-            get_density_altitude_text(),
-            '',
-            `Generated using <a href=${ADDRESS}>AVWX Scribe</a>`
-        ]
-        const title = `Information ${tags.join(" ")}`;
-        const html_code = `
-        <body style="background-color:black;color:white">
-        <title>${title}</title>
-        <h1>${title}</h1>
-        <div>
-        ${content.join("<br>")}
-        </div>
-        </body>
-        `
-        const file = new Blob([html_code], { type: 'html'});
-
-        // Add file content in the object URL
-        link.href = URL.createObjectURL(file);
-
-        // Add click event to tag to save file.
-        link.click();
-        URL.revokeObjectURL(link.href);
-    };
+            props.time,
+            props.altimeter,
+            props.windVel,
+            props.windCondition,
+            props.windGust,
+            props.windDir,
+            props.temperature,
+            props.dewpoint,
+        ], () =>
+        { update_atis_text(); }
+    );
 </script>
 
 <template>
     <p id="atisText">{{atisText}}</p>
-    <button @click="download_transcript" id="downloadButton"> Download Transcript </button>
+    <TranscriptDownloader
+        :atisText="atisText"
+        :information="information"
+        :altimeter="altimeter"
+        :airport="airport"
+        :visibility="visibility"
+        :cloudCoverage="cloudCoverage"
+        :ceiling="ceiling"
+        :time="time"
+        :windDir="windDir"
+        :windGust="windGust"
+        :windVel="windVel"
+        :windCondition="windCondition"
+        :dewpoint="dewpoint"
+        :temperature="temperature"
+        :densityAltitude="densityAltitude"
+    />
 </template>
 
 <style scoped>
---
+++ src/components/TranscriptDownloader.vue
@@ -0,0 +1,164 @@
+<script setup lang="ts">
+    import {ref, watch} from "vue"
+
+    const props = defineProps({
+        atisText: {type: String, required: true},
+        airport: {type: String, required: true},
+        information: {type: String, required: false},
+        time:  {type: String, required: true},
+        windCondition: {type: String, required: true},
+        windVel: {type: Number, required: true},
+        windDir: {type: Number, required: true},
+        windGust: {type: Number, required: true},
+        visibility: {type: Number, required: true},
+        cloudCoverage: {type: String, required: true},
+        ceiling: {type: Number, required: true},
+        temperature: {type: Number, required: true},
+        dewpoint: {type: Number, required: true},
+        altimeter:  {type: Number, required: true},
+        densityAltitude:  {type: Number, required: true},
+    });
+    const disablingCoverages = ["SKC", "NCD", "CLR", "VV"];
+
+    const ADDRESS = "https://mcgsjoyner.github.io/AVWX-Scribe/";
+
+    function get_airport_text() {
+        return `Airport: ${props.airport}`;
+    }
+
+    function get_information_text() {
+        return props.information ? `Information: ${props.information}` : null;
+    }
+
+    function get_time_text() {
+        return `Time: ${props.time} Z`;
+    }
+
+    function get_wind_text() {
+        var windText = 'Wind: ';
+        if (props.windVel == 0) {
+            windText += 'Calm';
+            return windText;
+        }
+        if (props.windCondition) {
+            windText += 'VRB';
+        }
+        else {
+            windText += props.windDir.toString().padStart(3, '0') + '&deg;'
+        }
+        let val = props.windVel.toFixed(0);
+        windText += ` @ ${val} KT`;
+        if (props.windGust > 0) {
+            val = props.windGust.toFixed(0);
+            windText += ` G ${val} KT`;
+        }
+        return windText;
+    }
+
+    function get_visibility_text() {
+        let val = props.visibility.toFixed(0);
+        return `Visibility: ${val} SM`;
+    }
+
+    function get_cloud_text() {
+        let cloudChunk = props.cloudCoverage;
+        if (!disablingCoverages.includes(props.cloudCoverage)) {
+            let val = Math.round(props.ceiling);
+            cloudChunk += `@ ${val} ft AGL`;
+        }
+        return `Cloud Coverage: ${cloudChunk}`;
+    }
+
+    function get_temperature_text() {
+        let val = props.temperature.toFixed(0);
+        return `Temperature: ${val}&deg;C`;
+    }
+
+    function get_dewpoint_text() {
+        let val = props.dewpoint.toFixed(0);
+        return `Dew Point: ${val}&deg;C`;
+    }
+
+    function get_spread_text() {
+        let spread = props.temperature - props.dewpoint;
+        let val = spread.toFixed(0);
+        return `Spread: ${val}&deg;C`
+    }
+
+    function get_altimeter_text() {
+        let val = props.altimeter.toFixed(2);
+        return `Altimeter Setting: ${val} inHg`;
+    }
+
+    function get_density_altitude_text() {
+        let val = props.densityAltitude.toFixed(0);
+        return `Density Altitude (est.): ${val} ft`;
+    }
+
+    function download_transcript() {
+        let isNotNull = (value: any) => value != null;
+
+        // Add file name
+        let tags = [
+            props.airport,
+            props.information,
+            props.time.replace(/\s/g, "") + 'Z',
+        ]
+        tags = tags.filter(isNotNull);
+
+        // Create a blob object with the file content which you want to add to the file
+        let lines = [
+            props.atisText,
+            '',
+            get_airport_text(),
+            get_information_text(),
+            get_time_text(),
+            get_wind_text(),
+            get_visibility_text(),
+            get_cloud_text(),
+            get_temperature_text(),
+            get_dewpoint_text(),
+            get_spread_text(),
+            get_altimeter_text(),
+            get_density_altitude_text(),
+            '',
+            `Generated using <a href=${ADDRESS}>AVWX Scribe</a>`
+        ]
+        let content = lines.filter(isNotNull).join("<br>");
+
+        const title = `Information ${tags.join(" ")}`;
+        const html_code = `
+            <body style="background-color:black;color:white">
+                <title>${title}</title>
+                <h1>${title}</h1>
+                <div>
+                    ${content}
+                </div>
+            </body>
+        `
+        const file = new Blob([html_code], { type: 'html'});
+
+        // Create element with <a> tag
+        const link = document.createElement("a");
+        link.download = `information-${tags.join('-')}.html`.toLowerCase();
+
+        // Add file content in the object URL
+        link.href = URL.createObjectURL(file);
+
+        // Add click event to tag to save file.
+        link.click();
+        URL.revokeObjectURL(link.href);
+    };
+
+    const styleObject = ({
+        marginBottom: "30px",
+        marginTop: "30px",
+        padding: "10px",
+    })
+</script>
+
+<template>
+    <button @click="download_transcript" id="downloadButton" :style="styleObject">
+        Download Transcript
+    </button>
+</template>
