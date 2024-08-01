c0ccd51c5836c434fac0535dae9b9889a671e9f5
McGregor Joyner <mcgsjoyner@gmail.com>
2024-08-01 12:06:31 -0600

    fix elevation picker color

--- src/components/ElevationPicker.vue
+++ src/components/ElevationPicker.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
-    import {ref} from "vue"
+    import {ref, reactive} from "vue"
 
-    const title = "Field Elevation (ft)"
+    const title = "Field Elevation (ft MSL)"
     // https://en.wikipedia.org/wiki/List_of_highest_airports
     const high = 14472;  // Daocheng Yading Airport
     const low = -1266;  // Bar Yehuda Airfield
     const start = 0;
-    const realValue = ref();
+    const realValue = ref(0);
+    const textColor = ref("var(--color-text-untouched)");
 
     function get_read_out() {
         return realValue.value.toFixed(0);
@@ -16,12 +17,17 @@
         (e: 'emitElevation', realValue: number): void
     }>()
     const onInput = () => {
-
+        textColor.value = "var(--color-text-dark)";
         realValue.value = Math.round(realValue.value)
         emit('emitElevation', realValue.value);
     }
-
-    onInput();
+    function initialize() {
+        emit('emitElevation', realValue.value);
+    }
+    const styleObject = reactive({
+        color: textColor,
+    })
+    initialize();
 </script>
 
 <template>
@@ -40,6 +46,7 @@
                 :max="high"
                 :min="low"
                 inputmode="numeric"
+                :style="styleObject"
             >
         </div>
     </div>
