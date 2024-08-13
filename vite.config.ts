758ee56d6e665e6d3897d2fbdee11f403d87d523
McGregor Joyner <mcgsjoyner@gmail.com>
2024-08-12 19:20:18 -0600

    try again

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,17 @@ import { VitePWA } from 'vite-plugin-pwa'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    __DATE__: `'${new Date().toISOString()}'`,
+  },
   plugins: [
     vue(),
     VitePWA(
         {
             registerType: 'autoUpdate',
-            includeAssets: [
-                './public/favicon.svg',
-                './public/apple-touch-icon-180-180.png',
-                './public/google-touch-icon-180-180.png'
-            ],
+            base: '/',
+            includeAssets: ['favicon.svg'],
             manifest: {
                 name: 'AV Scribe',
                 short_name: 'av-scribe',
@@ -24,25 +23,24 @@ export default defineConfig({
                 background_color: "#ffffff",
                 display: "fullscreen",
                 icons: [
-                    {
-                      src: "./public/favicon.svg",
-                      type: "image/svg+xml",
-                      purpose: "any"
-                    },
-                    {
-                      src: "./public/google-touch-icon.png",
+                  {
+                      src: "google-touch-icon.png",
                       sizes: "512x512",
                       type: "image/png",
-                      purpose: "any"
                   },
                   {
-                      src: "./public/apple-touch-icon-180-180.png",
+                      src: "apple-touch-icon-180-180.png",
                       sizes: "180x180",
                       type: "image/png",
-                      purpose: "maskable"
+                      purpose: "any maskable"
+                  },
+                  {
+                      src: "/apple-touch-icon-180-180.png",
+                      sizes: "180x180",
+                      type: "image/png",
                   }
                 ]
-            }
+            },
         }
     )
   ],
