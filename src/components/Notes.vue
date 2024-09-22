5276a75a1c67cab2e09adcad575e5a60dd8049a0
McGregor Joyner <mcgsjoyner@gmail.com>
2024-09-22 15:58:59 -0600

    add free form text entry field

---
+++ src/components/Notes.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+    import {ref, reactive} from "vue"
+    const title = "Notes"
+    const notes = ref('');
+
+    const textColor = ref("var(--color-text-untouched)");
+
+    const emit = defineEmits<{
+        (e: 'emitNotes', notes: string): void
+    }>()
+    const onChange = () => {
+        textColor.value = "var(--color-text)";
+        emit('emitNotes', notes.value);
+    }
+    const styleObject = reactive({
+        color: textColor,
+    })
+</script>
+
+<template>
+    <div class="inputContainer">
+        <label>
+            <span class="title">
+                {{ title }}:
+            </span>
+        </label>
+        <div class="inputArea">
+            <input
+                id="notesPicker"
+                type="text"
+                v-model.string="notes"
+                @change="onChange"
+                :style="styleObject"
+            >
+        </div>
+    </div>
+</template>
