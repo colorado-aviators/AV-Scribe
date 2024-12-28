ddbe403982e2bc6d504560255b7ba608c072e180
McGregor Joyner <mcgsjoyner@gmail.com>
2024-12-28 16:01:53 -0500

    remove deprecated code

--- src/lib/utils.ts
+++
@@ -1,8 +0,0 @@
-import * as math from 'mathjs'
-
-export function decodeFloat(min, max, code, depth){
-    let range = max - min;
-    let step = range / math.pow(2, depth);
-    var val = min + step * code;
-    return val
-}
