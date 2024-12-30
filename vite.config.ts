3cdb8c5a4a884a88ce29dfdf56c341679117d771
McGregor Joyner <mcgsjoyner@gmail.com>
2024-12-30 00:32:47 -0500

    try again

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
         {
             injectRegister: 'auto',
             registerType: 'autoUpdate',
-            base: '/',
+            base: '/AV-Scribe/',
             manifest: {
                 name: 'AV Scribe',
                 short_name: 'av-scribe',
