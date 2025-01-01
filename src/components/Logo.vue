46a837c683eea9fb13d58c3b3527ef6988dd36ab
McGregor Joyner <mcgsjoyner@gmail.com>
2025-01-01 18:14:14 -0500

    fix asset retrieval

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,17 +1,19 @@
 <script setup lang="ts">
     import {ref, watch} from "vue"
 
-    let logoRoot = "../src/assets/";
-    const filePathLight = logoRoot + "logo-light.svg";
-    const filePathDark = logoRoot + "logo-dark.svg";
-    const filePath = ref(filePathDark);
+    const fileNameLight = "logo-light";
+    const fileNameDark = "logo-dark";
+    const filePath = ref(getImageUrl(fileNameDark));
 
     const props = defineProps({
         userTheme: {type: String, required: true},
     });
     watch(() => props.userTheme, (newVal) => {
-        filePath.value = newVal == 'dark' ? filePathDark : filePathLight;
+        filePath.value = getImageUrl(newVal == 'dark' ? fileNameDark : fileNameLight);
     })
+    function getImageUrl(name: String) {
+        return new URL(`../assets/${name}.svg`, import.meta.url).href;
+    }
 </script>
 
 <template>
