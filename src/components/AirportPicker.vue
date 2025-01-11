9b91abeb69025fc372fb900ea4ac2b6de33d2f8b
McGregor Joyner <mcgsjoyner@gmail.com>
2025-01-10 22:29:00 -0800

    refactor

--- src/components/AirportPicker.vue
+++ src/components/AirportPicker.vue
@@ -1,11 +1,6 @@
 <script setup lang="ts">
     import {ref, reactive} from "vue"
     import * as airport_data from "../lib/fetch_airport_data"
-    const title = "Airport";
-
-    const airportID = ref('');
-
-    const textColor = ref("var(--color-text-untouched)");
 
     class GeocodeSystem{
         name: string;
@@ -16,17 +11,18 @@
         }
     }
 
+    const title = "Airport";
     const FAA = new GeocodeSystem("FAA", /^[A-Z0-9]{3,4}$/);
     const ICAO = new GeocodeSystem("ICAO", /^[A-Z]{4}$/);
-    const IATA = new GeocodeSystem("IATA", /^[A-Z]{4}$/);
-    const codices = ref([FAA, ICAO]);
-    const codex = ref(FAA);
+    const systemChoice = ref(FAA);
+    const airportID = ref('');
+    const textColor = ref("var(--color-text-untouched)");
 
     function format_airport(orig: string) {
         var val = orig.toUpperCase();
-        const regex = codex.value.regex;
+        const regex = systemChoice.value.regex;
         if (!regex.test(val)) {
-            alert(`Entry is not a valid ${codex.value.name} airport code`);
+            alert(`Entry is not a valid ${systemChoice.value.name} airport code`);
         }
         return val;
     }
@@ -37,14 +33,11 @@
     const onChange = () => {
         airportID.value = format_airport(airportID.value);
 
-        airport_data.loadAirportData(airportID.value, codex.value.name.toLowerCase()).then((airportData) => {
+        airport_data.loadAirportData(airportID.value, systemChoice.value.name.toLowerCase()).then((airportData) => {
             emit('emitAirport', airportData);
         }).catch((error) => console.error(error));
 
         textColor.value = "var(--color-text)";
-    }
-    const updateCodex = () => {
-        airportID.value = format_airport(airportID.value);
     }
     const styleObject = reactive({
         color: textColor,
@@ -69,17 +62,17 @@
                 :style="styleObject"
             >
             <div class="inputArea" @change="onChange">
-                <input type="radio" v-model="codex" :value="FAA" :id="FAA.name"/>
-                <label class="blah" :for="FAA.name">FAA</label>
-                <input type="radio" v-model="codex" :value="ICAO" :id="ICAO.name"/>
-                <label class="blah" :for="ICAO.name">ICAO</label>
+                <input type="radio" v-model="systemChoice" :value="FAA" :id="FAA.name"/>
+                <label class="system-picker" :for="FAA.name">FAA</label>
+                <input type="radio" v-model="systemChoice" :value="ICAO" :id="ICAO.name"/>
+                <label class="system-picker" :for="ICAO.name">ICAO</label>
             </div>
         </div>
     </div>
 </template>
 
 <style>
-    label.blah {
+    label.system-picker {
         font-size: 16px;
     }
 </style>
